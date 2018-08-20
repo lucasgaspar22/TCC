76d2066ce1eaddec2fa5b7913a688360dc84fabd
Lucas Gaspar <lucasprgaspar@gmail.com>
2018-08-20 16:51:09 -0300

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const bodyParser = require ('body-parser');
+const express = require('express');
+const app = express();
+const allowCors= require('./config/cors');
+
+const user = require('./routes/usuario')
+const is_friend = require('./routes/is_friend');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(allowCors);
+
+app.use('/API/user', user);
+app.use('/API/is_friend', is_friend);
+
+app.get('/',(req, res) =>{
+    res.send("Hello World");
+}); 
+
+
+
+app.listen(3003, function(){
+    console.log(`BACKEND is running on port ${3003}.`)
+});
+  
