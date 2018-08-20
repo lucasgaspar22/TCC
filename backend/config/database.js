53d3f72b5b668ac560d4e77c557afdb72b1de2a4
Lucas Gaspar <lucasprgaspar@gmail.com>
2018-08-20 16:53:07 -0300

    Create database.js

---
+++ backend/config/database.js
@@ -0,0 +1,31 @@
+var neo4j = require('neo4j');
+
+// Connect to DataBase
+var db = new neo4j.GraphDatabase('http://neo4j:1234@localhost:7474');
+
+
+function executeQuery(my_query,res){
+    db.cypher({
+        query: my_query
+    }, (err, results)=>{
+        if (err) throw err;
+        var result = results;
+        if (!result) {
+            res.err("999")
+        } else {
+           res.send(result);
+        }  
+    });
+}
+
+function callback(err, results) {
+    if (err) throw err;
+    var result = results;
+    if (!result) {
+        ERR = err
+    } else {
+       
+    }
+};
+
+module.exports = executeQuery;
