7367c16d43303652a5b2650659c5eff454c976aa
Lucas Gaspar <lucasprgaspar@gmail.com>
2018-08-22 17:46:31 -0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const allowCors= require('./config/cors');
 const user = require('./routes/usuario')
 const is_friend = require('./routes/is_friend');
 const asked_as_friend = require('./routes/asked_as_friend');
+const depoimento = require('./routes/depoimento');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.use(allowCors);
 app.use('/API/user', user);
 app.use('/API/is_friend', is_friend);
 app.use('/API/asked_as_friend', asked_as_friend);
+app.use('/API/depoimento',depoimento);
 
 app.get('/',(req, res) =>{
     res.send("Hello World");
