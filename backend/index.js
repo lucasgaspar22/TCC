5cef5d9a4f54031093b75bded9342fb1c4fb7713
Lucas Gaspar <lucasprgaspar@gmail.com>
2018-09-02 19:06:10 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const user = require('./routes/usuario')
 const is_friend = require('./routes/is_friend');
 const asked_as_friend = require('./routes/asked_as_friend');
 const depoimento = require('./routes/depoimento');
+const groups = require ( './routes/groups');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -16,10 +17,7 @@ app.use('/API/user', user);
 app.use('/API/is_friend', is_friend);
 app.use('/API/asked_as_friend', asked_as_friend);
 app.use('/API/depoimento',depoimento);
-
-app.get('/',(req, res) =>{
-    res.send("Hello World");
-}); 
+app.use('/API/groups',groups);
 
 
 
