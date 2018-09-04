156280ca00b0972528b7e88bd0e7fc122f026198
kisador <32437816+kisador@users.noreply.github.com>
2018-09-03 21:16:10 -0300

    Update is_friend.js

--- backend/routes/is_friend.js
+++ backend/routes/is_friend.js
@@ -2,77 +2,147 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../config/database');
+const formattedDate = require('../utils/date');
 
-// pega todas as relações IS_FRIEND de um nó ( Retorna todos os amigos de um determinado nó)
-router.get('/:id', (req,res,next)=>{
-    let id = parseInt (req.params.id);
-    let query = `MATCH (n:User)-[rel:IS_FRIEND]-(node:User) 
-                 WHERE id(n) = ${id} 
-                 RETURN rel,node`;
+//Método para pegar todos os grupos dos quais um usuário é membro
+router.get('/:idU/:pag', (req,res,next)=>{
+    let idU = parseInt(req.params.idU);
+    let pag = parseInt(req.params.pag);
+    let pagina = pag*5;
+    let query = `MATCH (user:User)-[rel:CREATED | :IS_MEMBER]->(group:Group)
+                WHERE id(user) = ${idU}
+                RETURN group,rel  SKIP ${pagina} LIMIT 5`;
+
     db(query,res);
 });
 
-// pega a relação IS_FRIEND entre dois nós
-router.get('/:id1/:id2', (req,res,next)=>{
-    let id1 = parseInt (req.params.id1);
-    let id2 = parseInt (req.params.id2);
+// Método para voltar todos os membros de um grupo
+router.get('/members/:idG/:pag',(req,res,next)=>{
+    let idG = parseInt(req.params.idG);
+    let pag = parseInt(req.params.pag);
+    let pagina = pag*5;
+    let query = `MATCH (user:User)-[rel:CREATED | :IS_MEMBER]->(group:Group)
+                 WHERE id(group) = ${idG}
+                 RETURN user,rel SKIP ${pagina} LIMIT 5`;
+    db(query,res)
+});
 
-    let query = `MATCH (node1:User)-[rel:IS_FRIEND]-(node2:User) 
-                 WHERE id(node1) = ${id1} AND id(node2) = ${id2} 
-                 RETURN node1,rel,node2`;
+//Método para ver todos os pedidos de participação feitos para um grupo
+router.get('/ask_membership/:idG/:pag', (req,res,next) =>{
+    let idG = parseInt(req.params.idG);
+    let pag = parseInt(req.params.pag);
+    let pagina = pag*5;
+    let query = `MATCH (user:User)-[rel:ASKED_MEMBERSHIP]->(group:Group)
+                 WHERE id(group) = ${idG} 
+                 RETURN user,rel SKIP ${pagina} LIMIT 5`;
     db(query,res);
 });
 
-// Cria uma relação IS_FRIEND entre dois nós
-router.post('/:id1/:id2', (req, res, next) =>{
-    let id1 = parseInt (req.params.id1);
-    let id2 = parseInt (req.params.id2);
-    let date = getFormattedDate();
-    let query = `MATCH (node1:User),(node2:User) 
-                 WHERE id(node1) = ${id1} AND id(node2) = ${id2} 
-                 CREATE (node1)-[rel:IS_FRIEND{since:"${date}"}]->(node2)
+//Método responsável por listar todos os convites recebidos por um usuário 
+router.get('/invite/:idU', (req,res,next) =>{
+    let idU = req.params.idU;
+    let pag = parseInt(req.params.pag);
+    let pagina = pag*5;
+    let query = `MATCH (group:Group)-[inv:INVITED]->(user:User) 
+                WHERE id(user) = ${idU} 
+                RETURN group,inv SKIP ${pagina} LIMIT 5`;
+
+    db(query,res);
+});
+
+//Método Responsável por criar um grupo
+router.post( '/:idU' , (req,res,next) => {
+    let name = req.body.nome;
+    let description = req.body.descricao;
+    let area = req.body.area;
+    let idCreator = parseInt(req.params.idU);
+    let date = formattedDate();
+
+    let query = `MATCH (creator:User) 
+                WHERE id(creator) = ${idCreator} 
+                CREATE (creator)-[rel:CREATED{data:"${date}"}]->(group:Group{nome:"${name}",descricao:"${description}",area:"${area}"})
+                RETURN creator,rel,group`;
+    db(query,res);
+
+});
+
+//Método para convidar uma pessoa no grupo 
+router.post('/invite/:idG/:idI', (req,res,next) =>{
+    let idG = parseInt(req.params.idG);
+    let idI = parseInt(req.params.idI);
+
+    let query = `MATCH (group:Group), (user:User)
+                 WHERE id(group) = ${idG} AND id(user)= ${idI} 
+                 CREATE (group)-[rel:INVITED{date:"${formattedDate()}"}]->(user)
                  RETURN rel`;
+
+    db(query,res);
+});
+
+ 
+// Método para pedir para participar de um grupo 
+router.post('/ask_membership/:idG/:idU',(req,res,next) =>{
+    let idG = parseInt(req.params.idG);
+    let idU = parseInt(req.params.idU);
+
+    let query  = `MATCH (group:Group),(user:User)
+                  WHERE id(group) = ${idG} AND id(user) = ${idU} 
+                  CREATE (user)-[rel:ASKED_MEMBERSHIP{data:"${formattedDate()}"}]->(group) 
+                  RETURN rel`;
+
+    db(query,res);
+});
+
+// Método para aceitar um pedido de participação
+router.put('/ask_membership/:idG/:idU',(req,res,next)=>{
+    let idG = parseInt(req.params.idG);
+    let idU = parseInt(req.params.idU);
+    
+    let query = `MATCH (user:User)-[asked:ASKED_MEMBERSHIP]->(group:Group)
+                 WHERE id(group) = ${idG} AND id(user) = ${idU} 
+                 DELETE asked 
+                 CREATE (user)-[rel:IS_MEMBER{date:"${formattedDate()}"}]->(group) 
+                 RETURN rel`;
+
+   db(query,res);
+});
+
+//Método para um convidado aceitar um convite do grupo
+router.put('/invite/:idG/:idI',(req,res,next)=>{
+    let idG = parseInt(req.params.idG);
+    let idI = parseInt(req.params.idI);
+    
+    let query = `MATCH (group:Group)-[inv:INVITED]->(user:User)
+                 WHERE id(group) = ${idG} AND id(user)=${idI}
+                 DELETE inv
+                 CREATE (user)-[rel:IS_MEMBER{date:"${formattedDate()}"}]->(group) 
+                 RETURN rel`;
+
     db(query,res);
 });
 
 
-// Deleta todas as relaçãoes IS_FRIENDS de um nó
-router.delete('/:id', (req, res, next) =>{
-    let id = parseInt(req.params.id);
+// Método responsável por apagar relaçoes entre um grupo e um usuário desde que essa relação seja
+// INVITED, ASKED_MEMBERSHIP ou IS_MEMBER
+router.delete('/:idG/:idU',(req,res,next)=>{
+    let idG = parseInt(req.params.idG);
+    let idU = parseInt(req.params.idU);
 
-    let query = `MATCH (node:User)-[rel:IS_FRIEND]-() 
-                WHERE id(node) = ${id}
+    let query = `MATCH (group:Group)-[rel:INVITED |:ASKED_MEMBERSHIP | :IS_MEMBER]-(user:User) 
+                WHERE id(group) = ${idG} AND id(user) = ${idU}
                 DELETE rel`;
+
     db(query,res);
 });
 
-
-// Deleta a relação IS_FRIEND entre dois nós
-router.delete('/:id1/:id2', (req, res, next) =>{
-    let id1 = parseInt(req.params.id1);
-    let id2 = parseInt(req.params.id2);
-
-    let query = `MATCH (node:User)-[rel:IS_FRIEND]-(node2:User) 
-                WHERE id(node) = ${id1} AND id(node2) = ${id2}
-                DELETE rel`;
+//Método responsável por deletar o grupo e todas as relações que o mesmo possui
+router.delete('/:idG', (req,res,next) => {
+    let idG = parseInt(req.params.idG);
+    let query = `MATCH (group:Group)-[rel:INVITED |:ASKED_MEMBERSHIP | :IS_MEMBER | :CREATED]-(user:User) 
+                WHERE id(group) = ${idG} 
+                DELETE rel
+                DELETE group`
     db(query,res);
 });
-
-
-function getFormattedDate(){
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth()+1;
-    let day = now.getDate();
-
-    let formattedDate = twoDigits(day) + "/" + twoDigits(month) + "/" + twoDigits(year);
-    return formattedDate;
-}
-
-function twoDigits(d) {
-    if(0 <= d && d < 10) return "0" + d.toString();
-    return d.toString();
-}
-
 
 module.exports = router;
