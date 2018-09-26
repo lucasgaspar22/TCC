d01674c6b08b1258a4f8ccd84f2eaa4b438a6025
Lucas Gaspar <lucasprgaspar@gmail.com>
2018-09-25 22:48:35 -0300

    Primeiras sessões do Perfil

--- tcc-front-end/src/app/dashboard/dashboard-main/perfil/perfil-service.service.ts
+++ tcc-front-end/src/app/dashboard/dashboard-main/perfil/perfil-service.service.ts
@@ -19,9 +19,27 @@ export class PerfilService {
     return this.http.get<any[]>(url_get_depos);
   }
 
+  getUserDeposNumbers(id:number){
+    let url_count_depos = `http://localhost:3003/API/user/get_depo_number/${id}`;
+
+    return this.http.get<any[]>(url_count_depos);
+  }
+
   getUserGroups(id:number, pag:number){
     let url_get_groups = `http://localhost:3003/API/groups/${String(id)}/${String(pag)}`;
 
     return this.http.get<any[]>(url_get_groups);
+  }
+
+  getUserGroupsNumbers(id:number){
+    let url_count_groups = `http://localhost:3003/API/user/get_group_number/${id}`;
+
+    return this.http.get<any[]>(url_count_groups);
+  }
+
+  getUserFriendsNumbers(id:number){
+    let url_count_friends = `http://localhost:3003/API/user/get_friend_number/${id}`;
+
+    return this.http.get<any[]>(url_count_friends);
   }
 }
--- tcc-front-end/src/app/dashboard/dashboard-main/perfil/perfil.component.ts
+++ tcc-front-end/src/app/dashboard/dashboard-main/perfil/perfil.component.ts
@@ -15,8 +15,13 @@ export class PerfilComponent implements OnInit, AfterViewInit {
 
   id:any;
   usuario:any = {};
+  depoimentos: any[] = [];
+  grupos: any [] = [];
   pag_depo:number = 0;
+  num_depo:number = 0 ;
   pag_group:number = 0;
+  num_group:number = 0;
+  num_friends:number = 0 ;
   relacao:any;
 
   constructor(private perfilService:PerfilService, private http:HttpClient, private route:ActivatedRoute, private toastr:ToastrService) { }
@@ -24,23 +29,50 @@ export class PerfilComponent implements OnInit, AfterViewInit {
    ngOnInit() {
     this.pag_depo = 0 ;
     this.pag_group = 0;
+    this.num_depo = 0 ;
+    this.num_group = 0 ;
     this.relacao = {};
     this.id = this.getUserId();
+
+    //Pega o usuário com id passado no parâmetro
     this.perfilService.getUserById(this.id).subscribe(res=>{
       this.usuario = res[0].node.properties;
-      console.log(this.usuario)
     });
+    
+    //Pega os depoimentos do usuário com o id passado no parametro
+    this.perfilService.getUserDepos(this.id, this.pag_depo).subscribe(res=>{
+      for( let index = 0; index< res.length; index++ ){
+        this.depoimentos.push(res[index]);
+      }
+    });
+
+    //Pega os grupos de que um usuário faz parte
+    this.perfilService.getUserGroups(this.id, this.pag_group).subscribe(res=>{
+      for( let index = 0; index< res.length; index++ ){
+        this.grupos.push(res[index]);
+      }
+      console.log(this.grupos);
+    });
+
+    //Pega o número total de depoimentos que um usuário tem 
+    this.perfilService.getUserDeposNumbers(this.id).subscribe(res=>{
+      this.num_depo = res[0].depos;
+    });
+
+    //Pega o número total de grupos que um usário tem 
+    this.perfilService.getUserGroupsNumbers(this.id).subscribe(res=>{
+      this.num_group = res[0].groups;
+    });
+
+    //Pega o número total de amigo que um usuário tem 
+    this.perfilService.getUserFriendsNumbers(this.id).subscribe(res=>{
+      this.num_friends = res[0].friends;
+    });
+
 
   }
 
   ngAfterViewInit(){
-    // Pega o Perfil do usuário
-    
-    //Pega os Depoimentos do usuário
-  
-    //Pega os Grupos do usuário
-
-    //Pega a relação entre o usuário logado e o usuário buscado
   }
 
   getUserId():number{
